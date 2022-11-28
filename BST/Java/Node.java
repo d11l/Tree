2ab84a4e72edfb5e38f14eb7044bafd220d4e08c
Abdulrahman radhi <80861137+d11l@users.noreply.github.com>
2022-11-28 19:17:22 +0300

    Create Node.java

---
+++ BST/Java/Node.java
@@ -0,0 +1,17 @@
+package tree;
+
+/**
+ * @Abdulrahman
+ */
+public class Node {
+    
+int value;
+Node Right;
+Node Left;
+
+ public Node(){}
+ public Node(int value) {
+        this.value = value;
+}
+  
+}
